583d5716bc1d6566c9b53430a83d17004d0b5bc2
AaronSala <salaaron2@gmail.com>
2023-02-09 10:31:43 +0300

    further progress

---
+++ newdir/index.js
@@ -0,0 +1,47 @@
+
+// var form2 = document.forms.myForm
+// var message=document.getElementById('message');
+
+// form2.name.onclick=function(){
+//     form2.name.style.border="4px solid red";
+// }
+// form2.name.onblur=function(){
+//      form2.name.style.border='none';
+//  }
+
+// form2.onsubmit=function(){
+//     if(form2.name.value ==""){
+//     //
+//     message.innerHTML='please fill the form';
+//     return false;
+//     }
+//     else{
+//         message.innerHTML='';
+//         return true;
+//     }
+//    };form2.name.onclick="window.print()";
+   
+//    //form2.style.display='none';
+
+
+
+//    var firstName="john", lastName="doe", age="35";
+//    console.log(firstName," ", lastName," ", age);
+//let date=(Date())
+
+// const cars=['bmw', 'toyota', 'nissan', 'volvo'];
+// for(i=0; i<cars.length;  i++) {
+// console.log(i,cars[i]);
+// }
+
+//var car=['bmw', 'toyota', 'nissan', 'volvo'];
+
+
+ const fruits = ["Apple", "Banana", "Orange"];
+ 
+ for (let x of fruits) 
+ 
+ {
+  console.log(x);
+
+ }
--- start/index.js
+++
@@ -1,4 +0,0 @@
-function changeText() {
-    var mytext= document.getElementById("mytext");
-    mytext.innerHTML = "you clicked me";
-}
