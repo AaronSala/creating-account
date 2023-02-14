84cb54476fec79e9bfc0692560202084781f769e
AaronSala <salaaron2@gmail.com>
2023-02-14 21:10:23 +0300

    new changes

--- forms/index.js
+++ forms/index.js
@@ -1,35 +1,52 @@
-var form=document.getElementById('valid');
-var name=document.getElementById('fname');
- var pass1=document.getElementById('password');
- var pass2=document.getElementById('password');
-var email=document.getElementById('email');
-var name2=document.getElementById('onames');
- var button=document.getElementById('button') ;
- const errorElement=document.getElementById('error');
- form.addEventListener('submit',(e)=>{
-   e.preventDefault();
-   checkInputs();
- })
- function checkInputs(){
-   const namevalue = fname.value.trim();
-   const emailvalue=email.value.trim();
-   const pass1value=pass.value.trim();
-   const pass2value=pass2.value.trim();
-   const name2value=name2.value.trim();
+// var form=document.getElementById('valid');
+// var name=document.getElementById('fname');
+//  var pass1=document.getElementById('password');
+//  var pass2=document.getElementById('password');
+// var email=document.getElementById('email');
+// var name2=document.getElementById('onames');
+//  var button=document.getElementById('button') ;
+//  const errorElement=document.getElementById('error');
+//  form.addEventListener('submit',(e)=>{
+//    e.preventDefault();
+//    checkInputs();
+//  })
+//  function checkInputs(){
+//    const namevalue = fname.value.trim();
+//    const emailvalue=email.value.trim();
+//    const pass1value=pass.value.trim();
+//    const pass2value=pass2.value.trim();
+//    const name2value=name2.value.trim();
 
-   if(name===''){
-  //error
-  setErorfor(name,'name cant be empty');
-   }
-   else{
-        setsuccessFor(name);
-   }
- }
- function setErrorFor(input, message){
-   const formControl=input.parentElement;
-   const small=formControl.querySelector('small');
-   small.innerText=message;
-   formControl.className='form-control error';
- }
+//    if(name===''){
+//   //error
+//   setErorfor(name,'name cant be empty');
+//    }
+//    else{
+//         setsuccessFor(name);
+//    }
+//  }
+//  function setErrorFor(input, message){
+//    const formControl=input.parentElement;
+//    const small=formControl.querySelector('small');
+//    small.innerText=message;
+//    formControl.className='form-control error';
+//  } myForm.fname.onclick()
+//   if(fname.value==''){
+//      error.innerHTML='name must be filled';
+//   };
 
+
+  myForm.fname.onclick=function(){
+        myForm.fname.style.border="4px solid red";
+       if(myForm.fname=''){
+
+       }
+//      }
+// document.getElementById.button.onclick=function(){
+//   if (myForm.fname.value==''){
+//     alert('input name');
+//   }
+// }
+const myForm=document.querySelector('form');
+const email=document.querySelector("input'email');
 
