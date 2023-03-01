8957a0e6b1972487573e18adc3af6c63ef9e6d10
AaronSala <salaaron2@gmail.com>
2023-03-01 11:27:36 +0300

    do more

--- calculator/calc.js
+++ calculator/calc.js
@@ -11,12 +11,11 @@ buttons.forEach(function(button){
 })
 });
 equal.addEventListener('click',function(e){
-    if(screen.value ===''){
+    if(screen.value ===undefined){
         screen.value ="";
     }else{
-         let answer = eval(screen.value);
-      screen.value=answer;
-        
+        let answer = eval(screen.value);
+        screen.value=answer;  
     }
 })
 
