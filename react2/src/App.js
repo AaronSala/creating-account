6e115ccde1001f5f52c7281810bfe38a78411970
AaronSala <salaaron2@gmail.com>
2023-03-07 13:12:04 +0300

    more practice

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,24 +1,21 @@
 
 import './App.css';
-import {useState} from 'react';
-const Person = (props)=>{
-  return (
-    <>
-    <h1>name: {props.name}</h1>
-    <h2>lastName: {props.lastName}</h2>
-    <h3>age:{props.age}</h3>
-    </>
-  )
-}
+import {useState ,useEffect} from 'react';
+
   
 
 const App=() =>{
- 
+  //using react hook
+  const[counter,setCounter] =useState(0);
+  useEffect(()=>{
+    
+  })
   return (
     <div className="App">
-      <Person name='john' lastName='bled' age={ 34 }/>
-      <Person name='mwende' lastName='mulwa'/>
-      <Person />
+      <button onClick={
+        ()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
     </div>
   );
 }
